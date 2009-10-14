01c5b9ab0292807919a0b5cf7ac0050db630b004
root <root@Azul.(none)>
2009-10-15 00:01:08 +0200

    added script insert_folder_in_filenames.sh

---
+++ insert_folder_in_filenames.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+#
+# insert_folder_in_filename.sh
+#
+# Copyright (C) 2009 Francisco Puga. http://conocimientoabierto.es
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# AUTHORS:
+# Francisco Puga Alonso <fran.puga@gmail.com>
+#
+# KEYWORDS: shell script, rename, directory tree, 
+#
+# DEPENDENCIES:
+#
+# DESCRIPTION: insert_folder_in_filename.sh search recursively (entering 
+# subdirectories) all files from an inicial directory and copy each file to 
+# a given directory with a name like 
+# <directory_name_that_contains the file>-filename
+#
+# CHANGELOG:
+#  14/10/2009. Creation
+
+
+usage() {
+    echo "$0 -i initial_directory -o output_directory"
+    echo
+    echo "DESCRIPTION: insert_folder_in_filename.sh search recursively (entering subdirectories) all files from an inicial directory and copy each file to a given directory with a name like <directory_name_that_contains the file>-filename"
+    echo "BE AWARE: if directories or files contain spaces in the names script may fail"
+    echo
+    echo "-i: The inicial directory where begin the search of files"
+    echo "-o: The outpu_directory where place the renamed files"
+    exit -1
+}
+
+
+indir='.'
+outdir='.'
+
+IFS='
+'
+
+for if in `find $indir -iname '*'` ; do
+    str=`dirname $if`
+    prefix=`expr "$str" : '.*\(/.*\)' | cut -d'/' -f2`
+    of=${prefix}-`basename $if`
+
+    cp "$if"  "${outdir}/$of"
+done
+
+
+while [ $# -gt 0 ] ; do
+    case $1
+        in
+        -i)
+            $indir=$2
+            shift 2
+            ;;
+
+
+        -o)
+            outdir=$2
+            shift 2
+            ;;
+
+        *)
+            usage
+            ;;
+    esac
+done
+
+
+
+if ! [ -d $outdir ] ; then echo "Destiny folder doesn't exist"; exit -1; fi
+
+for if in `find $indir -iname '*'` ; do
+    str=`dirname $if`
+    prefix=`expr "$str" : '.*\(/.*\)' | cut -d'/' -f2`
+    of=${prefix}-`basename $if`
+
+    cp "$if"  "${outdir}/$of"
+done
+
+exit 0
